cc7e6ae16cbff3430545700aca17c2bf8f27f5ed
Haoson <haoweiqinghaoson@gmail.com>
2014-09-29 16:58:08 -0700

    BST implementation

---
+++ data_structure/binary_search_tree.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+  > File Name: binary_search_tree.h
+  > Author:Haoson 
+  > Created Time: Mon 29 Sep 2014 04:57:26 PM PDT
+  > Description:二叉查找树的实现 
+ ************************************************************************/
+
+#ifndef BINARY_SEARCH_TREE_H_
+#define BINARY_SEARCH_TREE_H_
+#endif
