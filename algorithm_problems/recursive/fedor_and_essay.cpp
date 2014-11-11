08ff2dc689520c7c9275228846da549638bf8811
Haoson <haoweiqinghaoson@gmail.com>
2014-11-10 22:10:21 -0800

    add recursive problem:fedor and essay problem

---
+++ algorithm_problems/recursive/fedor_and_essay.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+  > File Name: fedor_and_essay.cpp
+  > Author:Haoson 
+  > Created Time: Mon 10 Nov 2014 12:54:57 AM PST
+  > Description:codeforces 467D -Fedor and Essay (http://codeforces.com/problemset/problem/467/D) 
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
