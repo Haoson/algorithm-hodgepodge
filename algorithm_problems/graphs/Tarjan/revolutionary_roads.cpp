bc516c33bd0a0cfa751695a9be53627de05e23dd
Haoson <haoweiqinghaoson@gmail.com>
2014-11-16 20:19:04 -0800

    add graph problem:revolutionary roads

---
+++ algorithm_problems/graphs/Tarjan/revolutionary_roads.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+  > File Name: revolutionary_roads.cpp
+  > Author:Haoson 
+  > Created Time: Sun 16 Nov 2014 08:14:26 PM PST
+  > Description:SGU 525Revolutionary Roads(http://acm.sgu.ru/problem.php?contest=0&problem=525) 
+ ************************************************************************/
+/**
+ *思路：Tarjan缩点，然后dfs
+ **/
+#include<iostream>
+using namespace std;
+
