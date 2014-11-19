0d7a2dd09475546cb23b172c0115f726960de659
Haoson <haoweiqinghaoson@gmail.com>
2014-11-18 19:57:43 -0800

    revolutionary roads

--- algorithm_problems/graphs/Tarjan/revolutionary_roads.cpp
+++ algorithm_problems/graphs/Tarjan/revolutionary_roads.cpp
@@ -24,19 +24,44 @@ static size_t road_num;
 static vector<Road> roads;
 static vector<vector<size_t> > adj_matrix;//图的邻接矩阵,adj_matrix[i][j]=1表示城市i能到达城市j,0表示不能到达
 static vector<bool> visited;
+static vector<size_t> rev_roads;//能变成revolutionary roads的道路的编号
+static size_t max_w=0;//最大强连通分量的节点个数
 
 void processInput();//处理输入
 void processOutput();//处理输出
-void preProcessAdjMatrix();
-void dfs(size_t city,size_t fa_city,vector<bool>&isVisited);
+void preProcessAdjMatrix();//对邻接矩阵进行预处理，处理出来每个节点能达到的所有节点（行）和能到此节点的所有节点（列）
+void dfs(size_t city,size_t fa_city,vector<bool>&isVisited);//dfs处理邻接矩阵
+void changeRoad();//将每条路变成revolutionary roads，查找最大强连通分量
 
 int main(int argc,char*argv[]){
     processInput();
     preProcessAdjMatrix(); 
+    changeRoad();
     processOutput();
     return 0;
 }
-
+void changeRoad(){
+    size_t temp = 0;
+    vector<size_t> temp_v;
+    for(auto it = roads.begin();it!=roads.end();++it){
+        //做交集
+        for(size_t i=1;i!=city_num+1;++i){
+            if((adj_matrix[(*it).from_city][i]&adj_matrix[i][(*it).from_city])){
+                ++temp;
+                temp_v.push_back(i);
+            }
+            if((adj_matrix[(*it).to_city][i]&adj_matrix[i][(*it).to_city])){
+                ++temp;
+                temp_v.push_back(i);
+            }
+        } 
+    }
+    if(temp>max_w){
+        max_w = temp;
+        rev_roads.clear();
+       rev_roads.insert(rev_roads.end(),temp_v.begin(),temp_v.end());
+    }
+}
 void dfs(size_t city,size_t fa_city,vector<bool>& isVisited){
     isVisited[city] = true;
     size_t a;
@@ -86,5 +111,6 @@ void processOutput(){
         } 
         cout<<endl;
     }
+    cout<<"max w="<<max_w<<endl;
 #endif
 }
