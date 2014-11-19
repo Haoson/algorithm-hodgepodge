10fc12a20e180245a5f6276ebeaef8d982c0941f
Haoson <haoweiqinghaoson@gmail.com>
2014-11-18 21:19:31 -0800

    fix all bugs

--- algorithm_problems/graphs/Tarjan/revolutionary_roads.cpp
+++ algorithm_problems/graphs/Tarjan/revolutionary_roads.cpp
@@ -10,10 +10,11 @@
 #include<iostream>
 #include<cstdio>
 #include<vector>
+#include<set>
 #include<algorithm>
 using namespace std;
 
-#define DEBUG
+//#define DEBUG
 
 struct Road{
     size_t id,from_city,to_city;
@@ -24,8 +25,9 @@ static size_t road_num;
 static vector<Road> roads;
 static vector<vector<size_t> > adj_matrix;//图的邻接矩阵,adj_matrix[i][j]=1表示城市i能到达城市j,0表示不能到达
 static vector<bool> visited;
-static vector<size_t> rev_roads;//能变成revolutionary roads的道路的编号
+static set<size_t> rev_roads;//能变成revolutionary roads的道路的编号
 static size_t max_w=0;//最大强连通分量的节点个数
+static size_t rev_road_id;//revolutionary road 编号
 
 void processInput();//处理输入
 void processOutput();//处理输出
@@ -42,24 +44,20 @@ int main(int argc,char*argv[]){
 }
 void changeRoad(){
     size_t temp = 0;
-    vector<size_t> temp_v;
     for(auto it = roads.begin();it!=roads.end();++it){
-        //做交集
-        for(size_t i=1;i!=city_num+1;++i){
-            if((adj_matrix[(*it).from_city][i]&adj_matrix[i][(*it).from_city])){
+        temp = 0;
+        for(size_t i=1;i!=city_num+1;++i){//做交集
+            if((adj_matrix[(*it).from_city][i])&(adj_matrix[i][(*it).from_city]|adj_matrix[i][(*it).to_city])){
                 ++temp;
-                temp_v.push_back(i);
-            }
-            if((adj_matrix[(*it).to_city][i]&adj_matrix[i][(*it).to_city])){
-                ++temp;
-                temp_v.push_back(i);
             }
         } 
-    }
-    if(temp>max_w){
-        max_w = temp;
-        rev_roads.clear();
-       rev_roads.insert(rev_roads.end(),temp_v.begin(),temp_v.end());
+        if(temp>max_w){
+            max_w = temp;
+            rev_roads.clear();
+            rev_roads.insert((*it).id);
+        }else if(temp==max_w){
+            rev_roads.insert((*it).id);
+        }
     }
 }
 void dfs(size_t city,size_t fa_city,vector<bool>& isVisited){
@@ -80,6 +78,7 @@ void preProcessAdjMatrix(){
     vector<bool> isVisited;
     isVisited.assign(city_num+1,false);
     for(size_t i=1;i!=city_num+1;++i){
+        adj_matrix[i][i] = 1;
         dfs(i,0,isVisited);
         visited[i] = true;
         std::fill(isVisited.begin(),isVisited.end(),false);
@@ -103,6 +102,12 @@ void processInput(){
 void processOutput(){
 #ifndef DEBUG
     freopen("/home/haoson/workspace/github-project/algorithm-hodgepodge/algorithm_problems/output/revolutionary_roads_output.txt","w",stdout);
+    cout<<max_w<<endl;
+    cout<<(rev_roads.size())<<endl;
+    for(auto it=rev_roads.begin();it!=rev_roads.end();++it){
+        cout<<(*it)<<" ";
+    }
+    cout<<endl;
 #else
     cout<<"dfs与处理之后的邻接图："<<endl;
     for(size_t i=0;i!=city_num+1;++i){
@@ -112,5 +117,10 @@ void processOutput(){
         cout<<endl;
     }
     cout<<"max w="<<max_w<<endl;
+    cout<<"可替换的rev road个数："<<(rev_roads.size())<<endl;
+    for(auto it=rev_roads.begin();it!=rev_roads.end();++it){
+        cout<<(*it)<<" ";
+    }
+    cout<<endl;
 #endif
 }
